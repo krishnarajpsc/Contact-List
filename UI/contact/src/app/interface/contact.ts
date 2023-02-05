500be8c8572a1bc8051bd03a243e9db7047895ac
Krishna <krish.ch@unite.care>
2023-02-05 19:13:25 +0530

    initial push

---
+++ UI/contact/src/app/interface/contact.ts
@@ -0,0 +1,12 @@
+import { Data } from '@angular/router';
+export interface contact{
+    FirstName?:string;
+    LastName?:string;
+    Email?:string;
+    PhoneNumber?:string;
+    Address?:string
+    City?:string;
+    State?:string;
+    Country?:string;
+    PostalCode?:string;
+}
